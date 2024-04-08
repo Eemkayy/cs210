5f141040712dead54ec4f37dc987bf912b8d0a6d
Eemkayy <maksims.kkes@gmail.com>
2024-04-08 14:50:48 -0400

    Random blabber
    Added a few statements and potential points to work on, going to centralize it more onto our hypothesis learning instead towards the next commits

--- FinalProjectCode.py
+++ FinalProjectCode.py
@@ -18,3 +18,17 @@ def genre_trend_per_year(df):
 
     plt.tight_layout()
     plt.show()
+
+def which_books_top_50_but_not_last_year(df, current_year, genre):
+    current_year_df = df[(df['Year'] == current_year) & (df['Genre'] == genre)]
+    previous_year_df = df[(df['Year'] == current_year - 1) & (df['Genre'] == genre)]
+    new_books_this_year = current_year_df[~current_year_df['Name'].isin(previous_year_df['Name'])]
+    
+    return new_books_this_year
+
+
+def average_price_per_genre_between_years(df, start_year, end_year):
+    filtered_df = df[(df['Year'] >= start_year) & (df['Year'] <= end_year)]
+    avg_price_per_genre_year = filtered_df.groupby(['Year', 'Genre'])['Price'].mean().unstack()
+    
+    return avg_price_per_genre_year
