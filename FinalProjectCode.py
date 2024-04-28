0a9b86f4db71c482bec36b563a109f6bcf60aa70
Eemkayy <maksims.kkes@gmail.com>
2024-04-27 20:40:46 -0400

    added commentary

--- FinalProjectCode.py
+++ FinalProjectCode.py
@@ -5,6 +5,18 @@ from matplotlib import pyplot as plt
 
 
 def pie_dist(df, pie_by='Genre'):
+    """
+    Creates a pie chart that visualizes the distribution of categories in the specified column. 
+    Categories with fewer than 2 occurrences are grouped into an "Others" category.
+    
+    Args:
+        df (DataFrame): The DataFrame containing the data to be visualized.
+        pie_by (str): Column name based on which the pie chart will be generated. Default is 'Genre'.
+    
+    Returns:
+        Displays a pie chart visualization.
+    """
+
     # Drop duplicates based on 'Name' and 'Author' to ensure uniqueness
     unique_df = df.drop_duplicates(subset=['Name', 'Author'])
 
@@ -27,7 +39,20 @@ def pie_dist(df, pie_by='Genre'):
     plt.show()
 
 def plot_price(df):
+    """
+    Generates a histogram of the 'Price' column in the DataFrame after removing duplicates based on 'Name' and 'Author'.
+
+    Args:
+        df (DataFrame): The DataFrame containing the data to be visualized.
+
+    Returns:
+        Displays a histogram.
+    """
+
+
+    # Dropping any possible duplicates
     df = df.drop_duplicates(subset=['Name', 'Author'])
+    #Creating the frequency distribution chart
     plt.figure(figsize=(10, 6))
     plt.hist(df['Price'], bins=20, color='blue', edgecolor='black')
     plt.title('Frequency Distribution of Prices')
@@ -37,7 +62,18 @@ def plot_price(df):
     plt.show()
 
 def plot_ratings(df):
+    """
+    Displays a histogram representing the frequency distribution of the 'User Rating' column after removing duplicates.
+
+    Args:
+        df (DataFrame): The DataFrame containing the data to be visualized.
+
+    Returns:
+        Displays a histogram.
+    """
+    #Removing duplicates
     df = df.drop_duplicates(subset=['Name', 'Author'])
+    #Plotting out the histogram.
     plt.figure(figsize=(10, 6))
     plt.hist(df['User Rating'], bins=5, color='red', edgecolor='black')
     plt.title('Frequency Distribution of User Rating')
@@ -47,7 +83,18 @@ def plot_ratings(df):
     plt.show()
 
 def plot_reviews(df):
+    """
+    Visualizes the frequency distribution of the 'Reviews' column in a histogram, using data that excludes duplicates.
+
+    Args:
+        df (DataFrame): The DataFrame containing the data to be visualized.
+
+    Returns:
+        Displays a histogram.
+    """
+    #Removing duplicates
     df = df.drop_duplicates(subset=['Name', 'Author'])
+    #Plotting out the histogram
     plt.figure(figsize=(10, 6))
     plt.hist(df['Reviews'], bins=30, color='orange', edgecolor='black')
     plt.title('Frequency Distribution of User Review Count')
@@ -56,7 +103,20 @@ def plot_reviews(df):
     plt.grid(True)
     plt.show()
 
+    # =======================================================================================================================
+
 def genre_trend_per_year(df):
+    """
+    Plots a stacked bar chart showing the number of books per genre over each year. 
+    Helps in understanding the trend of genres over time.
+    
+    Args:
+        df (DataFrame): The DataFrame containing the data to be visualized.
+    
+    Returns:
+        Displays a bar chart.
+    """
+    #We group the DF into a smaller one 
     genre_count_per_year = df.groupby(['Year', 'Genre']).size().unstack(fill_value=0)
     genre_count_per_year['Total'] = genre_count_per_year.sum(axis=1)
 
@@ -74,6 +134,19 @@ def genre_trend_per_year(df):
     plt.show()
 
 def which_books_top_50_but_not_last_year(df, current_year, genre):
+    """
+    Finds books that have entered the top 50 in a specific genre this year but were not in the top 50 the previous year.
+    
+    Args:
+        df (DataFrame): The DataFrame containing the data.
+        current_year (int): The year for which to find new top books.
+        genre (str): The genre to filter books by.
+    
+    Returns:
+        DataFrame: Contains books that are in the top 50 this year but were not last year in the given genre.
+    """
+
+
     current_year_df = df[(df['Year'] == current_year) & (df['Genre'] == genre)]
     previous_year_df = df[(df['Year'] == current_year - 1) & (df['Genre'] == genre)]
     new_books_this_year = current_year_df[~current_year_df['Name'].isin(previous_year_df['Name'])]
@@ -82,12 +155,33 @@ def which_books_top_50_but_not_last_year(df, current_year, genre):
 
 
 def average_price_per_genre_between_years(df, start_year, end_year):
+    """
+    Calculates the average price per genre for each year in the specified range and returns a DataFrame with these averages.
+    
+    Args:
+        df (DataFrame): The DataFrame containing the data.
+        start_year (int): The starting year for the analysis.
+        end_year (int): The ending year for the analysis.
+    
+    Returns:
+        DataFrame: Showing the average price per genre per year within the specified range.
+    """
+
     filtered_df = df[(df['Year'] >= start_year) & (df['Year'] <= end_year)]
     avg_price_per_genre_year = filtered_df.groupby(['Year', 'Genre'])['Price'].mean().unstack()
     
     return avg_price_per_genre_year
 
 def repeat_best_sellers(df):
+    """
+    Identifies books that appear multiple times in the dataset and displays a boxplot of reviews by genre for these repeat bestsellers.
+    
+    Args:
+        df (DataFrame): The DataFrame containing the data.
+    
+    Returns:
+        None. Displays a boxplot.
+    """
     repeat_bestsellers = df.groupby(['Name', 'Author']).filter(lambda x: len(x) > 1)
     fig, ax = plt.subplots(figsize=(10, 6))
     for genre, group in repeat_bestsellers.groupby('Genre'):
@@ -97,6 +191,17 @@ def repeat_best_sellers(df):
     plt.show()
 
 def correlation_cost_reviews_rating(df):
+
+    """
+    Displays a correlation matrix showing relationships between 'User Rating', 'Reviews', and 'Price'.
+    
+    Args:
+        df (DataFrame): The DataFrame containing the data.
+    
+    Returns:
+        None. Displays a correlation matrix.
+    """
+    
     correlation_matrix = df[['User Rating', 'Reviews', 'Price']].corr()
 
     fig, ax = plt.subplots()
@@ -112,6 +217,19 @@ def correlation_cost_reviews_rating(df):
     plt.show()
 
 def genre_performance_comparison (df):
+
+    """
+    Compares average user ratings and review counts by genre using a bar chart for ratings 
+    and a line chart for reviews on a secondary axis.
+    
+    Args:
+        df (DataFrame): The DataFrame containing the data.
+    
+    Returns:
+        None. Displays a dual-axis chart.
+    """
+
+
     genre_means = df.groupby('Genre').agg({'User Rating': 'mean', 'Reviews': 'mean'}).reset_index()
     genres = genre_means['Genre']
     ratings_mean = genre_means['User Rating']
@@ -134,9 +252,20 @@ def genre_performance_comparison (df):
 
 def analysis_price_cultural_relevance(df):
 
+    """
+    Analyzes the distribution of user reviews across different price ranges defined by custom bins.
+    
+    Args:
+        df (DataFrame): The DataFrame containing the data.
+    
+    Returns:
+        None. Displays a boxplot of reviews distribution across defined price ranges.
+    """
+
+
     df['Price Range'] = pd.cut(df['Price'], bins=[0, 10, 20, 30, np.inf], labels=['$0-10', '$10-20', '$20-30', '$30+'])
 
-    fig, ax = plt.subplots(figsize=(10, 6))
+    ax = plt.subplots(figsize=(10, 6))
     price_ranges = df['Price Range'].unique().dropna()
     reviews = [df[df['Price Range'] == price]['Reviews'] for price in price_ranges]
     ax.boxplot(reviews, labels=price_ranges)
@@ -146,6 +275,17 @@ def analysis_price_cultural_relevance(df):
 
 
 def plot_retention_rate(df):
+    """
+    Calculates and plots the retention rate of books from one year to the next.
+    
+    Args:
+        df (DataFrame): The DataFrame containing the data.
+    
+    Returns:
+        None. Displays a bar chart showing retention rates over years.
+    """
+
+
     grouped = df.groupby('Year')
     retention_rates = []
 
@@ -177,6 +317,24 @@ def plot_retention_rate(df):
     plt.show()
 
 def plot_retention_rate_by_criteria(df, group_by='Genre', group_by_title='Genre', cut_range=[0, 10, 20, np.inf], cut_labels=['$0-10', '$10-20', '$20+'], to_cut=False):
+    """
+    Calculates and plots the retention rate of books by a specified criterion over consecutive years. 
+    Allows for cutting the data into ranges for more granular analysis if required.
+
+    Args:
+        df (DataFrame): The DataFrame containing the data.
+        group_by (str): The column to group data by before calculating retention rates. Default is 'Genre'.
+        group_by_title (str): The label for the group by column in the plot. Default is 'Genre'.
+        cut_range (list): Bins for cutting the group_by column into discrete segments. Default is [0, 10, 20, np.inf].
+        cut_labels (list): Labels for the bins specified in cut_range. Default is ['$0-10', '$10-20', '$20+'].
+        to_cut (bool): Whether to cut the group_by column into discrete segments. Default is False.
+
+    Returns:
+        None. Displays a bar chart of retention rates by specified criteria.
+    """
+    
+    
+    
     if to_cut:
         df[f'{group_by} Range'] = pd.cut(df[group_by], bins=cut_range, labels=cut_labels)
         group_by = f"{group_by} Range"
@@ -204,7 +362,7 @@ def plot_retention_rate_by_criteria(df, group_by='Genre', group_by_title='Genre'
                     retention_data[year][field]['rate'] = retention_rate
                     retention_data[year][field]['count'] = current_count
     
-    fig, ax = plt.subplots(figsize=(12, 8))
+    ax = plt.subplots(figsize=(12, 8))
     width = 0.75 / len(crit)
     for i, field in enumerate(crit):
         rates = [retention_data[year][field]['rate'] for year in years[:-1]]
@@ -225,6 +383,16 @@ def plot_retention_rate_by_criteria(df, group_by='Genre', group_by_title='Genre'
     plt.show()
 
 def change_over_time(df):
+    """
+    Visualizes the year-over-year change in the number of books by genre using a bar chart.
+
+    Args:
+        df (DataFrame): The DataFrame containing the data.
+
+    Returns:
+        None. Displays a bar chart.
+    """
+
     books_per_year_genre = df.groupby(['Year', 'Genre']).size().unstack(fill_value=0)
 
     yoy_changes = books_per_year_genre.diff().fillna(0)
@@ -240,6 +408,15 @@ def change_over_time(df):
 
 
 def trend_nf(df):
+    """
+    Analyzes and visualizes trends in the popularity and pricing of Non Fiction books over the years.
+
+    Args:
+        df (DataFrame): The DataFrame containing the data, filtered for Non Fiction books.
+
+    Returns:
+        None. Displays a dual-axis chart showing both the number of top 50 books and average price trends for Non Fiction books.
+    """
     non_fiction_data = df[df['Genre'] == 'Non Fiction']
 
     books_per_year = non_fiction_data.groupby('Year')['Name'].count()
@@ -280,6 +457,15 @@ def trend_nf(df):
 
 
 def trend_f(df):
+    """
+    Analyzes and visualizes trends in the popularity and pricing of Fiction books over the years.
+
+    Args:
+        df (DataFrame): The DataFrame containing the data, filtered for Fiction books.
+
+    Returns:
+        None. Displays a dual-axis chart showing both the number of top 50 books and average price trends for Fiction books.
+    """
     non_fiction_data = df[df['Genre'] == 'Fiction']
 
     books_per_year = non_fiction_data.groupby('Year')['Name'].count()
@@ -312,6 +498,18 @@ def trend_f(df):
     plt.show()  
 
 def plot_author_longevity(df, label='Authors', type='Author'):
+    """
+    Analyzes and visualizes the longevity of authors on the bestseller list by counting 
+    the number of years each author appears in the data.
+
+    Args:
+        df (DataFrame): The DataFrame containing the data.
+        label (str): Descriptive label for the plot. Default is 'Authors'.
+        type (str): The column name to analyze. Default is 'Author'.
+
+    Returns:
+        Displays a bar chart showing the number of years authors have been on the bestseller list.
+    """
     # Remove rows with missing or empty strings
     author_years = {}
     for year, group in df.groupby('Year'):
@@ -340,7 +538,7 @@ def plot_author_longevity(df, label='Authors', type='Author'):
     plt.grid(axis='y', linestyle='--', alpha=0.7)
 
     # Add text labels for bars
-    for bar, duration in zip(bars, durations):
+    for bar, in zip(bars, durations):
         yval = bar.get_height()
         plt.text(bar.get_x() + bar.get_width() / 2, yval, str(yval), ha='center', va='bottom')
 
